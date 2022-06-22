2a807baa950078b19e9f5e36fe7f726eca6e6c42
Sabina Omarova <sabomar170@gmail.com>
2022-06-22 14:47:51 -0400

    .

--- src/sabina/Methods_Tasks.java
+++ src/sabina/Methods_Tasks.java
@@ -1,21 +1,34 @@
 package sabina;
 
+import java.util.Arrays;
+
 public class Methods_Tasks {
     public static void main(String[] args) {
+
+        System.out.println("============= sleepIn() ===============");
 
         System.out.println(sleepIn(false, false)); // true
         System.out.println(sleepIn(true, false));  //false
         System.out.println(sleepIn(false, true)); //true
 
-        System.out.println("======================");
+        System.out.println("============== lastDigit() =================");
         System.out.println(lastDigit(7, 17));
 
-        System.out.println("=================");
+        System.out.println("============== middleTwo() ==================");
         System.out.println(middleTwo("practice"));
 
-        System.out.println("=================");
-        int [] array = {1,2, 3,4};
+        System.out.println("================ countEvens() ================");
+        int[] array = {1, 2, 3, 4};
         System.out.println(countEvens(array));
+
+        System.out.println("===============withoutTen()====================");
+        int[] array2 = {10, 2, 10};
+        System.out.println(Arrays.toString(withoutTen(array2)));
+
+        System.out.println("============== catDog() =======================");
+
+        System.out.println(catDog("1cat1cadodog"));
+
 
     }
 
@@ -28,27 +41,26 @@ public class Methods_Tasks {
         }
     }
 
-    public static boolean lastDigit(int num1, int num2){
-        if(num1 %10 == num2 %10){
+    public static boolean lastDigit(int num1, int num2) {
+        if (num1 % 10 == num2 % 10) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public static String middleTwo(String str){ // practice
+    public static String middleTwo(String str) { // practice
 
         int divStr = str.length() / 2; //4
-        return str.substring(divStr-1,divStr+1);
+        return str.substring(divStr - 1, divStr + 1);
 
     }
 
-
-    public static int countEvens (int[] array){
+    public static int countEvens(int[] array) {
 
         int count = 0;
         for (int each : array) {
-            if(each % 2 == 0){
+            if (each % 2 == 0) {
                 count++;
 
             }
@@ -56,6 +68,52 @@ public class Methods_Tasks {
         }
         return count;
     }
+
+    public static int[] withoutTen(int[] array) {
+
+        int[] withoutTen = new int[array.length];
+
+        int i = 0;
+        for (int each : array) {
+            if (each != 10) {
+                withoutTen[i] = each;
+                i++;
+            }
+            if (each == 10) {
+                withoutTen[i] = 0;
+            }
+
+        }
+     return withoutTen;
+
+}
+
+    public static boolean catDog(String str){
+        int countCat = 0;
+        int countDog = 0;
+
+
+        for(int i = 0; i< str.length()-2; i++) {
+
+            if (str.substring(i, i + 3).equalsIgnoreCase("cat")) {
+                countCat++;
+
+            } else if (str.substring(i, i + 3).equalsIgnoreCase("dog")) {
+                countDog++;
+            }
+        }
+
+
+            if(countCat==countDog){
+                return true;
+            }else{
+                return false;
+            }
+    }
+
+
+
+
 
 
 }
