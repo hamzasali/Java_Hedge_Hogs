73ad95b83bc6c04e71a7d389b3ab93ce2772172d
Alena Elhajassaad <alenaelhajassaad@yahoo.com>
2022-06-22 12:40:00 -0400

    i created middleTwo

--- src/alena/middleTwo.java
+++ src/alena/middleTwo.java
@@ -5,17 +5,16 @@ public class middleTwo {
 
         String str="string";
 
+        System.out.println(middleTwo(str));
+
+    }
+
+    public static String middleTwo(String str){
         String result="";
         String half1=str.substring(0,str.length()/2);
         String half2=str.substring(str.length()/2);
 
         result+=half1.substring(half1.length()-1)+half2.substring(0,1);
-
-        System.out.println(result);
-
-
-
-
-
+        return result;
     }
 }
