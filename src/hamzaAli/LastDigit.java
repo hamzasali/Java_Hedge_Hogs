abea6b5018e34c3e40a886947b83d2df6d879a51
Hamza Ali <hamzamughal2145@gmail.com>
2022-06-22 12:14:58 -0400

    LastDigit method

--- src/hamzaAli/LastDigit.java
+++ src/hamzaAli/LastDigit.java
@@ -2,10 +2,10 @@ package hamzaAli;
 
 public class LastDigit {
     public static void main(String[] args) {
-        System.out.println(lastDigit(6, 17));
+        System.out.println(lastDigit(57, 27));
     }
 
     public static boolean lastDigit(int n1, int n2) {
-        return n1 == n2 % 10;
+        return n1 % 10 == n2 % 10;
     }
 }
