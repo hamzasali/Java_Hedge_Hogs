b0b1de33fca66e89c8431e9b58659dccd14fd69e
Hamza Ali <hamzamughal2145@gmail.com>
2022-06-29 13:03:46 -0400

    .

--- src/hamzaAli/Circle.java
+++ src/hamzaAli/Circle.java
@@ -7,41 +7,58 @@ public class Circle {
     //and numberOfCircle(keep the info of how many object was created from this class).
     //decide the type of your variables (static or instance)
 
+    private double radius;
+    private int numberOfCircles;
 
-
-
+    private static double pi = 3.14;
 
     //create default and parameterized constructors
     //default constructor should increase the number of object when a new object created
+
+    public Circle() {
+    }
 
 
     //parameterized constructor should increase the number of object when a new object created.
     //and not allowed to initialize object with the radius value lower than Zero.
 
+    public Circle(double radius) {
+        setRadius(radius);
+    }
+
 
     //create getter and setter methods for radius.
     //getter
 
-
-
+    public double getRadius() {
+        return radius;
+    }
 
 
     //setter if the value of radius is lower than zero keep the previous value.
+
+    public void setRadius(double radius) {
+        if (radius < 0) {
+            radius = this.radius;
+        }
+        this.radius = radius;
+    }
 
 
     //do not allow any object to change value of static variable just get the current
     //value of numberOfCircle
 
 
-
-
-
     //calculate area of Circle
 
-
-
-
+    public double calcArea() {
+        return pi * (radius * radius);
+    }
 
     //calculate perimeter of Circle
+
+    public double calcPerimeter() {
+        return 2 * pi * radius;
+    }
 
 }
---
+++ src/hamzaAli/TestCircle.java
@@ -0,0 +1,13 @@
+package hamzaAli;
+
+public class TestCircle {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+
+        circle.setRadius(2);
+
+        System.out.println(circle.calcArea());
+
+    }
+}
