b83e396a04648bb317836a8062a2d5ca47bb18e2
Sergiy Baskov <master.flock@gmail.com>
2022-07-13 21:18:08 +0300

    My solution

--- src/accessModifiers/package2/Class5.java
+++ src/accessModifiers/package2/Class5.java
@@ -2,10 +2,11 @@ package accessModifiers.package2;
 
 import accessModifiers.package1.Class1;
 
+import accessModifiers.package1.Class1;
 public class Class5 {
     public static void main(String[] args) {
-//        Class1 class1 = new Class1();
-//        System.out.println(class1.x);
+       Class1 class1 = new Class1();
+        System.out.println(class1.x);
 //        System.out.println(class1.y);
 //        System.out.println(class1.z);
 //        System.out.println(class1.u);
