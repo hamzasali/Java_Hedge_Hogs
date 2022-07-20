2747b672dabbc43bdee23a33317f2970865a8fae
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-07-20 18:56:43 +0200

    house task is done

---
+++ src/week_14/TryCatch.java
@@ -0,0 +1,63 @@
+package week_14;
+
+public class TryCatch {
+
+    public static void main(String[] args) {
+//        try {
+//            System.out.println("This is a try block");
+//            throw new RuntimeException("Throw runtime exception here");
+//        } catch (RuntimeException e) {
+//          e.printStackTrace();
+//        }
+
+
+
+//        try {
+//            System.out.println("This is a try block");
+//            throw new RuntimeException("Throw runtime exception here");
+//        } catch (RuntimeException e) {
+//            e.printStackTrace();
+//        }finally {
+//            System.out.println("This is a finally block");
+//        }
+
+//        try {
+//            System.out.println("This is a try block");
+//            throw new StringIndexOutOfBoundsException("Throw exception here");
+//        } catch (NullPointerException e) {
+//            System.out.println("Null pointer exception catch block");
+//            e.printStackTrace();
+//        }catch (RuntimeException e) {
+//            System.out.println("Runtime exception catch block");
+//            e.printStackTrace();
+//        }catch (Exception e) {
+//            System.out.println("Exception catch block");
+//            e.printStackTrace();
+//        }
+
+
+//        try{
+//            System.out.println("Try block");
+//        }finally {
+//            System.out.println("Finally block");
+//        }
+
+
+//        try {
+//            System.out.println("This is a try block");
+//            throw new StringIndexOutOfBoundsException("Throw exception here");
+//        } catch (NullPointerException e) {
+//            System.out.println("Null pointer exception catch block");
+//            e.printStackTrace();
+//        } catch (RuntimeException e) {
+//            System.out.println("Runtime exception catch block");
+//            e.printStackTrace();
+//        } catch (Exception e) {
+//            System.out.println("Exception catch block");
+//            e.printStackTrace();
+//        }finally {
+//            System.out.println("It will work");
+//        }
+
+    }
+}
