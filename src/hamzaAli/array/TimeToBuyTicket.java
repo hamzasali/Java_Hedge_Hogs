57944bdf5ae76eca26f4e1bc70bd87428ef2fcf0
Hamza Ali <hamzamughal2145@gmail.com>
2022-10-26 11:23:42 -0400

    Time to Buy Tickets

--- src/hamzaAli/array/TimeToBuyTicket.java
+++ src/hamzaAli/array/TimeToBuyTicket.java
@@ -1,25 +1,56 @@
 package hamzaAli.array;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
 public class TimeToBuyTicket {
     public static void main(String[] args) {
-        int[] nums = {2, 3, 3, 2};
-        System.out.println(timeRequiredToBuy(nums, 2));
+        int[] nums = {5, 1, 1, 1};
+        System.out.println(timeRequiredToBuy(nums, 0));
     }
 
     public static int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
+//        int time = 0;
+//
+//        do {
+//            for (int i = 0; i < tickets.length; i++) {
+//                if (tickets[i] != 0) {
+//                    tickets[i] -= 1;
+//                    time++;
+//                }
+//                if (tickets[k] == 0) {
+//                    break;
+//                }
+//            }
+//        } while (tickets[k] != 0);
+//
+//        return time;
 
-        do {
-            for (int i = 0; i < tickets.length; i++) {
-                if (tickets[i] != 0) {
-                    tickets[i] -= 1;
-                    time++;
+        Queue<Integer> queue = Arrays.stream(tickets)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayDeque::new));
+        int totalTime = 0;
+        while (!queue.isEmpty()) {
+            int leftTicketCount = queue.poll() - 1;
+            totalTime++;
+            if (leftTicketCount == 0) {
+                if (k == 0) {
+                    return totalTime;
+                } else if (k > 0) {
+                    k--;
                 }
-                if (tickets[k] == 0) {
-                    break;
+
+            } else {
+                queue.add(leftTicketCount);
+                if (k > 0) {
+                    k--;
+                } else if (k == 0) {
+                    k = queue.size() - 1;
                 }
             }
-        } while (tickets[k] != 0);
-        return time;
+        }
+        return totalTime;
     }
 }
