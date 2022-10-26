48866bdac132015ffddf8d71b4cb2059afca1696
Hamza Ali <hamzamughal2145@gmail.com>
2022-10-26 11:24:38 -0400

    Array Single Number

--- src/hamzaAli/array/SingleNumber.java
+++ src/hamzaAli/array/SingleNumber.java
@@ -1,5 +1,6 @@
 package hamzaAli.array;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 
@@ -9,14 +10,26 @@ public class SingleNumber {
         System.out.println(singleNumber(nums));
     }
 
-    public static int singleNumber(int[] nums) { //O(n)
+    public static int singleNumber(int[] nums) { //time:O(n),space:O(n)
         HashSet<Integer> set = new HashSet<>();
         for (int each : nums) {
-            if (!set.add(each)) {
-                set.remove(each);
+            if (!set.add(each)) { //O(1)
+                set.remove(each); //O(1)
             }
         }
         return set.iterator().next();
 //        return (int) set.toArray()[0];
     }
+    public static int singleNumber1(int[] nums){ //time:O(n^2),space:O(n)
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (list.contains(num)){
+                list.remove(num);
+            }else {
+                list.add(num);
+            }
+        }
+        return list.get(0);
+    }
+
 }
