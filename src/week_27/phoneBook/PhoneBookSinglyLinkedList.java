666a59ae952137443d79341fcf7fadcbc8f1165e
Hamza Ali <hamzamughal2145@gmail.com>
2022-10-26 11:54:36 -0400

    PhoneBook Task

--- src/week_27/phoneBook/PhoneBookSinglyLinkedList.java
+++ src/week_27/phoneBook/PhoneBookSinglyLinkedList.java
@@ -7,11 +7,21 @@ public class PhoneBookSinglyLinkedList {
     public int size;
 
 
-    public boolean isEmpty(){
-        return head==null;
+    public boolean isEmpty() {
+        return head == null;
     }
-    public int size(){
+
+    public int size() {
         return size;
     }
 
+    public void printPhoneBook() {
+        PhoneBookNode current = head;
+        Integer element = 1;
+        while (current != null) {
+            System.out.println(element +". "+ current.contact);
+            element++;
+            current = current.next;
+        }
+    }
 }
