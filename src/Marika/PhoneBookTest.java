f0b579c179eaa2f382b4deae822cd119666c9e2a
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-11-11 17:17:35 +0100

    MyTreeApplication.java added

--- src/Marika/PhoneBookTest.java
+++ src/Marika/PhoneBookTest.java
@@ -2,14 +2,14 @@ package Marika;
 
 public class PhoneBookTest {
     public static void main(String[] args) {
-        PhoneBookSinglyLinkedList list = new PhoneBookSinglyLinkedList();
-
-
-        list.add(new Contact("David", "Mike", "mike@gmail.com", "123456782"));
-        list.add(new Contact("Emma", "Smith", "emma@gmail.com", "987654321"));
-        System.out.println(list.isEmpty());
-        list.printPhoneBook();
-        System.out.println(list.size());
+//        PhoneBookSinglyLinkedList list = new PhoneBookSinglyLinkedList();
+//
+//
+//        list.add(new Contact("David", "Mike", "mike@gmail.com", "123456782"));
+//        list.add(new Contact("Emma", "Smith", "emma@gmail.com", "987654321"));
+//        System.out.println(list.isEmpty());
+//        list.printPhoneBook();
+//        System.out.println(list.size());
 
 
     }
---
+++ src/week_29/tree/MyTreeApplication.java
@@ -0,0 +1,19 @@
+package week_29.tree;
+
+public class MyTreeApplication {
+
+    public static void main(String[] args) {
+
+        MyTree tree = new MyTree();
+
+        tree.insert(10);
+        tree.insert(6);
+        tree.insert(18);
+        tree.insert(3);
+        tree.insert(8);
+        tree.insert(22);
+
+        VisualizeTree.printTree(tree.root,null,false);
+
+    }
+}
