b0a8e507853551e576cee54e44032ac82e8a576b
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-11-27 14:28:16 +0100

    week_32

--- src/week_32/lowest_common_ancestor/Trunk.java
+++ src/week_32/lowest_common_ancestor/Trunk.java
@@ -1,9 +1,9 @@
 package week_32.lowest_common_ancestor;
 
 public class Trunk {
-    Trunk prev;
-    String str;
-    Trunk(Trunk prev, String str)
+    public Trunk prev;
+   public String str;
+   public  Trunk(Trunk prev, String str)
     {
         this.prev = prev;
         this.str = str;
