f949aa6111bd2b7318c3cb64041b87d4fc1489f9
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-13 12:29:51 -0500

    Wk34 task2

--- src/hamzaAli/wk34/Task2.java
+++ src/hamzaAli/wk34/Task2.java
@@ -1,4 +1,25 @@
 package hamzaAli.wk34;
 
+import java.util.Arrays;
+
 public class Task2 {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(multiplyElements(arr)));
+    }
+
+    static int[] multiplyElements(int[] arr) {
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                result[i] = arr[i] * arr[i + 1];
+            } else if (i == arr.length - 1) {
+                result[i] = arr[i] * arr[i - 1];
+            } else {
+                result[i] = arr[i - 1] * arr[i + 1];
+            }
+        }
+        return result;
+    }
 }
