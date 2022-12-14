495d927f27a625c032e8b565765170317e6b5bb2
Hamza Ali <hamzamughal2145@gmail.com>
2022-12-14 13:01:39 -0500

    Wk34 Words Frequency

--- src/hamzaAli/wk34/Task1.java
+++ src/hamzaAli/wk34/Task1.java
@@ -6,17 +6,18 @@ import java.util.Map;
 public class Task1 {
 
     public static void main(String[] args) {
-        String str = "This is an example of example";
+        String str = "This      is an example!!!!                  @@@@ of Example....";
         System.out.println(FrequencyOfWords(str));
     }
 
     static Map<String, Integer> FrequencyOfWords(String str) {
+        //Only for regular words string
         Map<String, Integer> map = new HashMap<>();
 
         if (str == null) return null;
         if (str.isEmpty()) return map;
 
-        String[] s = str.split(" ");
+        String[] s = str.toLowerCase().split("\\W+");
         int count = 1;
         for (String value : s) {
             if (map.containsKey(value)) {
@@ -26,5 +27,6 @@ public class Task1 {
         }
         return map;
     }
+
 
 }
